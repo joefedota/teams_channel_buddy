a0d6f0b3dce60de0cd1beda8b990332038321394
Raymond C <814499+usakiki@users.noreply.github.com>
2023-09-14 15:20:35 -0700

    Update HomeController.cs

--- RSCDemo/Controllers/HomeController.cs
+++ RSCDemo/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
 using RSCDemo.Helpers;
-using RSCDemo.Models;
 using RSCWithGraphAPI.Models;
 using System;
 using System.Collections.Generic;
@@ -26,25 +25,18 @@ namespace RSCWithGraphAPI.Controllers
         }
 
         /// <summary>
-        /// Hackathon
+        /// RSC Setup
         /// </summary>
-        [Route("Hackathon")]
-        public IActionResult Hackathon()
+        [Route("Demo")]
+        public async Task<ActionResult> Demo(string tenantId, string groupId, string channelId)
         {
-            // 1. call Graph API
-            //string content = ...
+            GraphServiceClient graphClient = await GetAuthenticatedClient(tenantId);
+            List<string> messages = await TempWrapper(graphClient, tenantId, groupId, channelId);
 
-            // 2. call Open AI
-            GetCompletionResponse();
-
-            return View();
-        }
-
-        private async Task<ViewResult> GetCompletionResponse()
-        {
-            //var client = new OpenAIClient(new Uri("https://americasopenai.azure-api.net"), new AzureKeyCredential("8f9931f759ea41a59f181b82535736ce"));
-            string content = "Please summarize below paragraph\n" +
-                "Manage a bot\r\nArticle\r\n11/15/2022\r\n8 contributors\r\nIn this article\r\nAzure Bot resource settings\r\nApplication service settings\r\nNext steps\r\nAPPLIES TO: SDK v4\r\n\r\nIn your browser, navigate to the Azure portal. Select your bot resource, such as an Azure Bot. On the navigation pane you'll see the sections described below.\r\n\r\nAzure Bot resource settings\r\nThe Azure Bot resource contains the settings described below.\r\n\r\nGeneral\r\nAt the top of the navigation pane are links for general information applicable to a bot.\r\n\r\nLink\tDescription\r\nOverview\tContains high level information about the bot, such as a bot's Subscription ID and Messaging endpoint. On the overview pane, you can also download the bot source code.\r\nActivity log\tProvides detailed diagnostic and auditing information for Azure resources and the Azure platform they depend on. For more information, see Overview of Azure platform logs.\r\nAccess control (IAM)\tDisplays the access users or other security principals have to Azure resources. For more information, see View the access a user has to Azure resources.\r\nTags\tDisplays the tags to your Azure resources, resource groups, and subscriptions to logically organize them into a taxonomy. For more information, see Use tags to organize your Azure resources.\r\nSettings\r\nIn the Settings section are links for most of your bot's management options.\r\n\r\nLink\tDescription\r\nBot profile\tManages various bot profile settings, such as display name, icon, and description.\r\nConfiguration\tManages various bot settings, such as analytics, messaging endpoint, and OAuth connection settings.\r\nChannels\tConfigures the channels your bot uses to communicate with users.\r\nPricing\tManages the pricing tier for the bot service.\r\nTest in Web Chat\tUses the integrated Web Chat control to quickly test your bot.\r\nEncryption\tManages your encryption keys.\r\nProperties\tLists your bot resource properties, such as resource ID, subscription ID, and resource group ID.\r\nLocks\tManages your resource locks.\r\nMonitoring\r\nIn the Monitoring section are links for most of your bot's monitoring options.\r\n\r\nLink\tDescription\r\nConversational analytics\tEnables analytics to view the collected data with Application Insights. This Analytics blade will be deprecated. For more information, see Add telemetry to your bot and Analyze your bot's telemetry data.\r\nAlerts\tConfigure alert rules and attend to fired alerts to efficiently monitor your Azure resources. For more information, see Overview of alerts in Microsoft Azure.\r\nMetrics\tSelect a metric to see data in the proper chart.\r\nDiagnostic settings\tDiagnostic settings are used to configure streaming export of platform logs and metrics for a resource to the destination of your choice. For more information,see diagnostic settings.\r\nLogs\tProduce insights from Azure Monitor logs.\r\nApplication service settings\r\nA bot application, also known as an application service (App Service), has a set of application settings that you can access through the Azure portal. They're environment variables passed to the bot application code. For more information, see Configure an App Service app in the Azure portal.\r\n\r\nIn your browser, navigate to the Azure portal.\r\nSearch for your bot app service and select its name.\r\nThe bot app service information is displayed.\r\n\r\nBot identity information\r\nFollow these steps to add identity information to your bot's configuration file. The file differs depending on the programming language you use to create the bot.\r\n\r\n Important\r\n\r\nThe Java and Python versions of the Bot Framework SDK only support multi-tenant bots. The C# and JavaScript versions support all three application types for managing the bot's identity.\r\n\r\nLanguage\tFile name\tNotes\r\nC#\tappsettings.json\tSupports all three application types for managing your bot's identity.\r\nJavaScript\t.env\tSupports all three application types for managing your bot's identity.\r\nJava\tapplication.properties\tOnly supports multi-tenant bots.\r\nPython\tconfig.py\tOnly supports multi-tenant bots. Provide the identity properties as arguments to the os.environ.get method calls.\r\nThe identity information you need to add depends on the bot's application type. Provide the following values in your configuration file.\r\n\r\nUser-assigned managed identity\r\nSingle-tenant\r\nMulti-tenant\r\nOnly available for C# and JavaScript bots.\r\n\r\nProperty\tValue\r\nMicrosoftAppType\tUserAssignedMSI\r\nMicrosoftAppId\tThe client ID of the user-assigned managed identity.\r\nMicrosoftAppPassword\tNot applicable. Leave this blank for a user-assigned managed identity bot.\r\nMicrosoftAppTenantId\tThe tenant ID of the user-assigned managed identity.\r\nNext steps\r\nNow that you've explored the Bot Service blade in the Azure portal, learn about the Bot Framework Service, how bots communicate with users, and about activities, channels, HTTP POST requests, and more.";
+            //
+            string content = "Please summarize below\n" +
+                messages[0].ToString();
+            //"Manage a bot\r\nArticle\r\n11/15/2022\r\n8 contributors\r\nIn this article\r\nAzure Bot resource settings\r\nApplication service settings\r\nNext steps\r\nAPPLIES TO: SDK v4\r\n\r\nIn your browser, navigate to the Azure portal. Select your bot resource, such as an Azure Bot. On the navigation pane you'll see the sections described below.\r\n\r\nAzure Bot resource settings\r\nThe Azure Bot resource contains the settings described below.\r\n\r\nGeneral\r\nAt the top of the navigation pane are links for general information applicable to a bot.\r\n\r\nLink\tDescription\r\nOverview\tContains high level information about the bot, such as a bot's Subscription ID and Messaging endpoint. On the overview pane, you can also download the bot source code.\r\nActivity log\tProvides detailed diagnostic and auditing information for Azure resources and the Azure platform they depend on. For more information, see Overview of Azure platform logs.\r\nAccess control (IAM)\tDisplays the access users or other security principals have to Azure resources. For more information, see View the access a user has to Azure resources.\r\nTags\tDisplays the tags to your Azure resources, resource groups, and subscriptions to logically organize them into a taxonomy. For more information, see Use tags to organize your Azure resources.\r\nSettings\r\nIn the Settings section are links for most of your bot's management options.\r\n\r\nLink\tDescription\r\nBot profile\tManages various bot profile settings, such as display name, icon, and description.\r\nConfiguration\tManages various bot settings, such as analytics, messaging endpoint, and OAuth connection settings.\r\nChannels\tConfigures the channels your bot uses to communicate with users.\r\nPricing\tManages the pricing tier for the bot service.\r\nTest in Web Chat\tUses the integrated Web Chat control to quickly test your bot.\r\nEncryption\tManages your encryption keys.\r\nProperties\tLists your bot resource properties, such as resource ID, subscription ID, and resource group ID.\r\nLocks\tManages your resource locks.\r\nMonitoring\r\nIn the Monitoring section are links for most of your bot's monitoring options.\r\n\r\nLink\tDescription\r\nConversational analytics\tEnables analytics to view the collected data with Application Insights. This Analytics blade will be deprecated. For more information, see Add telemetry to your bot and Analyze your bot's telemetry data.\r\nAlerts\tConfigure alert rules and attend to fired alerts to efficiently monitor your Azure resources. For more information, see Overview of alerts in Microsoft Azure.\r\nMetrics\tSelect a metric to see data in the proper chart.\r\nDiagnostic settings\tDiagnostic settings are used to configure streaming export of platform logs and metrics for a resource to the destination of your choice. For more information,see diagnostic settings.\r\nLogs\tProduce insights from Azure Monitor logs.\r\nApplication service settings\r\nA bot application, also known as an application service (App Service), has a set of application settings that you can access through the Azure portal. They're environment variables passed to the bot application code. For more information, see Configure an App Service app in the Azure portal.\r\n\r\nIn your browser, navigate to the Azure portal.\r\nSearch for your bot app service and select its name.\r\nThe bot app service information is displayed.\r\n\r\nBot identity information\r\nFollow these steps to add identity information to your bot's configuration file. The file differs depending on the programming language you use to create the bot.\r\n\r\n Important\r\n\r\nThe Java and Python versions of the Bot Framework SDK only support multi-tenant bots. The C# and JavaScript versions support all three application types for managing the bot's identity.\r\n\r\nLanguage\tFile name\tNotes\r\nC#\tappsettings.json\tSupports all three application types for managing your bot's identity.\r\nJavaScript\t.env\tSupports all three application types for managing your bot's identity.\r\nJava\tapplication.properties\tOnly supports multi-tenant bots.\r\nPython\tconfig.py\tOnly supports multi-tenant bots. Provide the identity properties as arguments to the os.environ.get method calls.\r\nThe identity information you need to add depends on the bot's application type. Provide the following values in your configuration file.\r\n\r\nUser-assigned managed identity\r\nSingle-tenant\r\nMulti-tenant\r\nOnly available for C# and JavaScript bots.\r\n\r\nProperty\tValue\r\nMicrosoftAppType\tUserAssignedMSI\r\nMicrosoftAppId\tThe client ID of the user-assigned managed identity.\r\nMicrosoftAppPassword\tNot applicable. Leave this blank for a user-assigned managed identity bot.\r\nMicrosoftAppTenantId\tThe tenant ID of the user-assigned managed identity.\r\nNext steps\r\nNow that you've explored the Bot Service blade in the Azure portal, learn about the Bot Framework Service, how bots communicate with users, and about activities, channels, HTTP POST requests, and more.";
 
             var chatCompletionsOptions = new ChatCompletionsOptions()
             {
@@ -57,30 +49,12 @@ namespace RSCWithGraphAPI.Controllers
                 deploymentOrModelName: "gpt-35-turbo-16k",
                 chatCompletionsOptions);
 
-            var viewModel = new HackathonViewModel()
-            {
-                //Response = response.Value.Choices[0].Message.Content
-                //Console.WriteLine($"Chatbot: {completion}");
-
-                Response = DateTime.Now.ToString() // to test tab navagation would trigger this event
-            };
-
-            return View(viewModel);
-        }
-
-        /// <summary>
-        /// RSC Setup
-        /// </summary>
-        [Route("Demo")]
-        public async Task<ActionResult> Demo(string tenantId, string groupId, string channelId)
-        {
-
-            GraphServiceClient graphClient = await GetAuthenticatedClient(tenantId);
             var viewModel = new DemoViewModel()
             {
+                Content = content,
+                Response = response.Value.Choices[0].Message.Content,
                 //Channels = await GetChannelsList(graphClient, tenantId, groupId),
                 //Permissions = await GetPermissionGrants(graphClient, tenantId, groupId)
-                Permissions = await TempWrapper(graphClient, tenantId, groupId, channelId),
             };
             return View(viewModel);
         }
@@ -122,8 +96,8 @@ namespace RSCWithGraphAPI.Controllers
 
         private async Task<List<string>> TempWrapper(GraphServiceClient graphClient, string tenantId, string groupId, string channelId)
         {
-            var messages = await GetMessagesList(graphClient, tenantId, groupId, channelId);
-            return new List<string>() { messages.Messages[0].ToString() };
+            var messageHistory = await GetMessagesList(graphClient, tenantId, groupId, channelId);
+            return new List<string>() { messageHistory.ToString() };
         }
 
         /// <summary>
