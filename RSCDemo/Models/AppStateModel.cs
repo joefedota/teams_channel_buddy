c29dda515618af46b299baccf2ef6ac41efef59b
sojanmatt28 <142056896+sojanmatt28@users.noreply.github.com>
2023-09-14 07:48:05 -0700

    Update AppStateModel.cs

--- RSCDemo/Models/AppStateModel.cs
+++ RSCDemo/Models/AppStateModel.cs
@@ -1,1 +1,11 @@
 
+namespace RSCDemo.Models
+{
+    public class AppStateModel
+    {
+        public int ChannelId { get; set; }
+        public string Summary { get; set; }
+        public string ActionItem { get; set; }
+        public string LastMessage { get; set; }
+    }
+}
