2006d133e481aa0923aa57b2b13fa196c758b8bb
Raymond C <814499+usakiki@users.noreply.github.com>
2023-09-14 08:40:39 -0700

    Update MessageHistory.cs

--- RSCDemo/Helpers/MessageHistory.cs
+++ RSCDemo/Helpers/MessageHistory.cs
@@ -1,6 +1,13 @@
-﻿namespace TeamsChannelBuddy.Helpers
+namespace TeamsChannelBuddy.Helpers
 {
     public class MessageHistory
     {
+
+        //wrapper for message history will implement method for MessageHistory.CleanHistory
+        //private List<CustomMessage> messages { get; set; }
+
+        public MessageHistory() 
+        { }
+
     }
 }
