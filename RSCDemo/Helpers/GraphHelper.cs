ec11225a7054a7201d0e61f4baba69526a084b11
Raymond C <814499+usakiki@users.noreply.github.com>
2023-09-14 08:40:13 -0700

    Update GraphHelper.cs

--- RSCDemo/Helpers/GraphHelper.cs
+++ RSCDemo/Helpers/GraphHelper.cs
@@ -1,6 +1,52 @@
-﻿namespace TeamsChannelBuddy.Helpers
+using Microsoft.Graph;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RSCDemo.Helpers
 {
     public class GraphHelper
     {
+        public static void WriteToJsonFile<T>(string filePath, T objectToWrite, bool append = false) where T : new()
+        {
+            TextWriter writer = null;
+            try
+            {
+                var contentsToWriteToFile = JsonConvert.SerializeObject(objectToWrite);
+                writer = new StreamWriter(filePath, append);
+                writer.Write(contentsToWriteToFile);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+
+        public static async Task<PageIterator<ChatMessage>> GetMessagesIterator(GraphServiceClient graphClient, string tenantId, string groupId, string channelId, List<CustomMessage> messages)
+        {
+
+            var result = await graphClient.Teams[groupId].Channels[channelId].Messages
+                .Request()
+                .GetAsync();
+
+
+            var pageIterator = PageIterator<ChatMessage>
+                .CreatePageIterator(
+                    graphClient,
+                    result,
+                    (msg) =>
+                    {
+                        CustomMessage newMessage = new CustomMessage(JsonConvert.SerializeObject(msg), msg.LastModifiedDateTime);
+                        //TODO: replace with MessageHistory Object so can run Clean from controller
+                        messages.Add(newMessage);
+                        //populate object to pass to OpenAI helper with required fields
+                        return true;
+                    });
+
+            //return result.Select(r => r.Body.Content).ToList();
+            return pageIterator;
+        }
     }
 }
